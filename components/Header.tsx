3ae0a41445510c905eb811908fa2635e3a09e5ad
Shaco74 <kacper74@hotmail.com>
2023-10-12 12:22:14 +0200

    Changed lang to german, improved layout, added title, added first post

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,37 +9,42 @@ import SearchButton from './SearchButton'
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
-      <div>
+      <div className="flex items-center justify-between w-full">
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
               <Logo />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
+              <div className="flex-col w-full">
+                <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+                <p className="text-lg leading-7 text-gray-500 dark:text-gray-400 pt-2">
+                  {siteMetadata.description}
+                </p>
               </div>
             ) : (
               siteMetadata.headerTitle
             )}
           </div>
         </Link>
-      </div>
-      <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
-            >
-              {link.title}
-            </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
+        <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
+              >
+                {link.title}
+              </Link>
+            ))}
+          <SearchButton />
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
       </div>
     </header>
   )
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,10 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: `Barcelona's Blog`,
+  author: 'Die Angler',
+  headerTitle: `Barcelona's Blog`,
+  description: 'Ein Blog um sie alle zu knechten',
+  language: 'de-eu',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
@@ -17,7 +17,7 @@ const siteMetadata = {
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  locale: 'de-eu',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
